6f1fb023e0637050011177da1ed3cf6729aaa629
Ahmad Akil <ferozbinakil@gmail.com>
2025-05-01 21:34:35 +0600

    Filtered Data

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
-    const {id} = useParams();
-    return (
-        <div>
-            Category News - {id}
-        </div>
-    );
+  const { id } = useParams();
+  const data = useLoaderData();
+  // console.log(id, data);
+
+  const [categoryNews, setCategoryNews] = useState([]);
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(data);
+    } else if (id == "1") {
+      const filteredNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filteredNews);
+    } else {
+      const filteredNews = data.filter((news) => news.category_id == id);
+      console.log(filteredNews);
+      setCategoryNews(filteredNews);
+    }
+  }, [data, id]);
+
+  
+  return <div>Total {categoryNews.length} news found</div>;
 };
 
 export default CategoryNews;
